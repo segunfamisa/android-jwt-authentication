4ddfb7a1e8e46554b3e19244a6b5699463052cef
Segun Famisa <segunfamisa@gmail.com>
2016-11-06 12:24:37 +0100

    chore: added logic to decode username from token

--- app/src/main/java/com/segunfamisa/sample/jwt/AuthHelper.java
+++ app/src/main/java/com/segunfamisa/sample/jwt/AuthHelper.java
@@ -5,9 +5,21 @@ import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import com.auth0.jwt.JWTVerifier;
 import com.segunfamisa.sample.jwt.network.Token;
 
+import java.util.Map;
+
 public class AuthHelper {
+    /**
+     * Secret used to verify the JWT token
+     */
+    private static final String SECRET= "ngEurope rocks!";
+
+    /**
+     * Key for username in the jwt claim
+     */
+    private static final String JWT_KEY_USERNAME = "username";
 
     private static final String PREFS = "prefs";
     private static final String PREF_TOKEN = "pref_token";
@@ -34,12 +46,37 @@ public class AuthHelper {
     }
 
     @Nullable
-    public String getIdToken() {
+    private String getIdToken() {
         return mPrefs.getString(PREF_TOKEN, null);
     }
 
     public boolean isLoggedIn() {
         String token = getIdToken();
         return token != null;
+    }
+
+    /**
+     * Gets the username of the signed in user
+     * @return - username of the signed in user
+     */
+    public String getUsername() {
+        if (isLoggedIn()) {
+            return decodeUsername(getIdToken());
+        }
+        return null;
+    }
+
+    @Nullable
+    private String decodeUsername(String token) {
+        JWTVerifier verifier = new JWTVerifier(SECRET);
+        try {
+            Map<String, Object> claims = verifier.verify(token);
+            if (claims != null && claims.containsKey(JWT_KEY_USERNAME)) {
+                return claims.get(JWT_KEY_USERNAME).toString();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
